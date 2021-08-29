be336a16240b83ba5aa05381f055be7bff731f45
sora-sea <yasu.abc.jda@gmail.com>
2021-08-29 23:13:20 +0900

    APIデータの読み取り

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 'use strict'
 
 {
-  
+  const quizTitle = document.getElementById('quiztitle');
+  const quizInfo = document.getElementById('quizinfo');
+  const quiz = document.getElementById('quiz');
+  const addBtn = document.getElementById('btn');
+  const anserList = document.getElementById('anserlist');
+  const apiUrl = 'https://opentdb.com/api.php?amount=10';
+
+  addBtn.addEventListener('click', async () => {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    console.log(data);
+  });
 }
