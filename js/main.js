afa9d541442f3196d429506cd20bf8dbd1c2c5c1
sora-sea <yasu.abc.jda@gmail.com>
2021-09-05 09:50:53 +0900

    現状のコード

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 'use strict'
 
 {
-  const quizTitle = document.getElementById('quiztitle');
-  const quizInfo = document.getElementById('quizinfo');
-  const quiz = document.getElementById('quiz');
-  const addBtn = document.getElementById('btn');
-  const anserList = document.getElementById('anserlist');
+  const title = document.getElementById('title');
+  const genre = document.getElementById('genre');
+  const difficulty = document.getElementById('difficulty');
+  const question = document.getElementById('question');
+  const addBtn = document.getElementById('startbtn');
+  const answers = document.getElementById('answers');
   const apiUrl = 'https://opentdb.com/api.php?amount=10';
 
   addBtn.addEventListener('click', async () => {
+    title.innerText = '取得中';
+    question.innerText = '少々お待ちください';
     const res = await fetch(apiUrl);
     const data = await res.json();
     console.log(data);
