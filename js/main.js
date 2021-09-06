d216725115bf78186a5fd5ac1c349fa64648cce8
sora-sea <yasu.abc.jda@gmail.com>
2021-09-07 08:34:44 +0900

    クラスを用意してAPIの配列の値を取得できるように書き足したコード

--- js/main.js
+++ js/main.js
@@ -9,11 +9,32 @@
   const answers = document.getElementById('answers');
   const apiUrl = 'https://opentdb.com/api.php?amount=10';
 
-  addBtn.addEventListener('click', async () => {
+  class Quiz {
+    constructor(data) {
+      this.quizzes = data.results;
+      this.correctAnswersNum = 0;
+    }
+    getCategory(index) {
+      return this.quizzes[index -1].category;
+    }
+    getDifficulty(index) {
+      return this.quizzes[index -1].difficulty;
+    }
+    getQuestion(index) {
+      return this.quizzes[index -1].question;
+    }
+  }
+
+  const fetchData = async (index) => {
     title.innerText = '取得中';
     question.innerText = '少々お待ちください';
-    const res = await fetch(apiUrl);
-    const data = await res.json();
-    console.log(data);
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      const instance = new Quiz(data);
+      setNextQuiz(instance, index);
+  }
+
+  addBtn.addEventListener('click', () => {
+
   });
 }
