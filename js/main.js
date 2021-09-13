860bdb515dd372c02eb3c53f3e0ea23113ef4057
sora-sea <yasu.abc.jda@gmail.com>
2021-09-13 21:58:03 +0900

    回答ボタン修正しました

--- js/main.js
+++ js/main.js
@@ -23,6 +23,12 @@
     getQuestion(index) {
       return this.quizzes[index -1].question;
     }
+    getCorrectAnswer(index) {
+      return this.quizzes[index -1].correct_answer;
+    }
+    getIncorrectAnswers(index) {
+      return this.quizzes[index -1].incorrect_answers;
+    }
   }
 
   const fetchData = async (index) => {
@@ -44,14 +50,20 @@
       const query = quiz.getQuestion(index);
       question.innerText = `${query}`;
 
+      const correctAnswer = quiz.getCorrectAnswer(index);
       const answerBtn1 = document.createElement('button');
-      // answerBtn1.innerText = `${correct_answer}`;
+      answerBtn1.innerText = `${correctAnswer}`;
       answers.appendChild(answerBtn1);
+
+      const incorrectAnswers = quiz.getIncorrectAnswers(index);
       const answerBtn2 = document.createElement('button');
+      answerBtn2.innerText = `${incorrectAnswers}`;
       answers.appendChild(answerBtn2);
       const answerBtn3 = document.createElement('button');
+      answerBtn3.innerText = `${incorrectAnswers}`;
       answers.appendChild(answerBtn3);
       const answerBtn4 = document.createElement('button');
+      answerBtn4.innerText = `${incorrectAnswers}`;
       answers.appendChild(answerBtn4);
 
       addBtn.style.display = 'none';
